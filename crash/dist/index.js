f3192f4b4ef30934700483420e49ea571fb837a2
Apple <apple@Apples-MacBook-Air.local>
2022-11-14 09:16:03 +0100

    typescript

---
+++ crash/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Basic Types in TypeScript
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+x = true;
+x = 1;
+let ids = [1, 2, 3, 4, 5];
---
+++ crash/src/index.ts
@@ -0,0 +1,13 @@
+// Basic Types in TypeScript
+
+let id: number = 5
+
+let company: string = 'Traversy Media'
+
+let isPublished: boolean = true
+
+let x: any = 'Hello'
+ 
+x = true
+x = 1
+ let ids: number[]= [1,2,3,4,5]
